cac1caafb8d8486af72db2d0f404681221a87e61
Andrew Pillow-Edwards <loophole.edwards@gmail.com>
2022-06-07 21:21:38 +0100

    Add comment to isStringTimeEarlier

--- helpers/isStringTimeEarlier.js
+++ helpers/isStringTimeEarlier.js
@@ -1,5 +1,7 @@
-const getStringAsTime = require("./getStringAsTime");
+// If there are more helper methods needed for handling string time, then consider
+// consolidating them into a single logic module.
 
+const getStringAsTime = require("./getStringAsTime");
 
 const isStringTimeEarlier = (stringTime1, stringTime2) => (
     getStringAsTime(stringTime1).isBefore(getStringAsTime(stringTime2))
